a865d7660af2e0dd0fabed7a034b24ecb0437c6e
Ellen Winther Mortensen <fuji@winther-mortensen.dk>
2025-02-20 10:11:38 +0000

    create singular test

---
+++ tests/assert_total_payment_amount_is_positive.sql
@@ -0,0 +1,7 @@
+--test that checks if the total amount for every row in the order model is positive
+select 
+    order_id, 
+    sum(amount) as total_amount
+from {{ ref('orders') }}
+group by 1
+having total_amount < 0
---
+++ tests/assert_unique_customers_customer_id.sql
@@ -0,0 +1,7 @@
+--test that check if every row in the customers model is unique
+
+select
+    customer_id
+from {{ ref('customers') }}
+group by 1
+having count(*) > 1 --find customers with the same customer_id
